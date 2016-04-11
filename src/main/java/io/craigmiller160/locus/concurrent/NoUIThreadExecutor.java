3363b8c29c253dfc86909eff05a7f7d74db4c25d
craigmiller160 <craigmiller160@gmail.com>
2016-04-11 08:37:05 -0400

    LBG-1 - Created NoUIThreadExecutor
    + Created NoUIThreadExecutor, the default, non-concurrent implementation of the UIThreadExecutor interface.

---
+++ src/main/java/io/craigmiller160/locus/concurrent/NoUIThreadExecutor.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2016 Craig Miller
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.craigmiller160.locus.concurrent;
+
+import java.util.concurrent.Callable;
+
+/**
+ * The default implementation of the UIThreadExecutor interface.
+ * This is a non-concurrent implementation, it executes the provided
+ * tasks on the same thread they came from.
+ *
+ * Created by craigmiller on 4/11/16.
+ */
+public class NoUIThreadExecutor implements UIThreadExecutor {
+
+    @Override
+    public void executeOnUIThread(Runnable task) {
+        task.run();
+    }
+
+    @Override
+    public <T> T executeOnUIThreadWithResult(Callable<T> task) {
+        T result = null;
+        try{
+            result = task.call();
+        }
+        catch(Exception ex){
+            //TODO figure out how to handle this exception
+        }
+
+        return result;
+    }
+}
