38ae5192c31a8abf830afc09d3dc1dbe7f34dc07
craigmiller160 <craigmiller160@gmail.com>
2016-04-12 16:28:20 -0400

    LBG-1 - Created Test for UIThreadExecutorFactory
    + Created UIThreadExecutorFactoryTest JUnit test class to test the UIThreadExecutorFactory class.

---
+++ src/test/java/io/craigmiller160/locus/concurrent/UIThreadExecutorFactoryTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2016 Craig Miller
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.craigmiller160.locus.concurrent;
+
+import io.craigmiller160.locus.sample.SampleUIThreadExecutor;
+import io.craigmiller160.locus.util.LocusStorage;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * A JUnit test class for testing the
+ * UIThreadExecutorFactory class.
+ *
+ * Created by craigmiller on 4/12/16.
+ */
+public class UIThreadExecutorFactoryTest {
+
+    private UIThreadExecutorFactory factory;
+    private LocusStorage storage;
+
+    /**
+     * Create the factory instance to be tested.
+     */
+    private void createFactory(){
+        factory = new UIThreadExecutorFactory(storage);
+    }
+
+    /**
+     * Create the LocusStorage instance for this test class.
+     */
+    private void createStorage(){
+        try{
+            Constructor<LocusStorage> constructor = LocusStorage.class.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            storage = constructor.newInstance();
+
+            storage.setUIThreadExecutorType(SampleUIThreadExecutor.class);
+        }
+        catch(InstantiationException | InvocationTargetException | NoSuchMethodException | IllegalAccessException ex){
+            throw new RuntimeException("Critical exception while trying to setup test", ex);
+        }
+    }
+
+    /**
+     * Run setup methods before each test.
+     */
+    @Before
+    public void begin(){
+        createStorage();
+        createFactory();
+    }
+
+    /**
+     * Test getting the correct UIThreadExecutor instance
+     * from the factory.
+     */
+    @Test
+    public void testGetUIThreadExecutor(){
+        UIThreadExecutor executor = factory.getUIThreadExecutor();
+
+        assertNotNull("UIThreadExecutor instance is null", executor);
+        assertEquals("UIThreadExecutor is the wrong type", SampleUIThreadExecutor.class, executor.getClass());
+    }
+
+}
